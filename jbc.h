331b39844441ad355fd525d9ebe3b6dc8bec450f
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2026-08-13 19:13:32 +0200

    feat: init

---
+++ jbc.h
@@ -0,0 +1,24 @@
+#ifndef JBC_H
+#define JBC_H
+
+#include <stddef.h>
+
+typedef struct {
+	int         fd;
+	size_t      size;
+	size_t      len;
+	size_t      max;
+	const void* map;
+} jbc;
+
+int    jbc_init(jbc* db, const char* path, size_t size, size_t max);
+int    jbc_fini(jbc* db);
+
+size_t jbc_len (const jbc* db);
+size_t jbc_size(const jbc* db);
+
+int    jbc_get (const jbc* db, size_t idx, void* data);
+int    jbc_ref (const jbc* db, size_t idx, const void** ref);
+int    jbc_add (jbc* db, size_t* idx, const void* data);
+
+#endif
